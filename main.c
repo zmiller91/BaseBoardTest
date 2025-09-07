ce16f006695dd8f8b68b06302282cad098bc0514
zmiller91 <me@zackmiller.info>
2025-09-07 15:28:43 -0500

    Changing to remap to V0.2 pins

--- main.c
+++ main.c
@@ -20,14 +20,14 @@
 #include "mcc_generated_files/system/config_bits.h"
 
 void task(void) {
-    LED_SetHigh();
+    LED_EN_SetHigh();
     log_debug("Hello from the application!");
     lora_enable();
     
     double data[] = {123.456};
     lora_send(32, data, 1);
     
-    LED_SetLow();
+    LED_EN_SetLow();
 }
 
 int main(void) {
