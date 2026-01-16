fba385c628a7115cd8e8eac0c0ca98d69becd4e6
zmiller91 <me@zackmiller.info>
2026-01-15 21:13:21 -0600

    Upgrading the API to reduce program size

--- main.c
+++ main.c
@@ -16,18 +16,40 @@
 #include "run.h"
 #include "log.h"
 #include "lora.h"
+#include "string_utils.h"
 #include <stdio.h>
+#include <stdint.h>
 #include "mcc_generated_files/system/config_bits.h"
 
+
+
+
 void task(void) {
+    
+    char addr_txt[6];
+    
+
+    char buff1[] = "AT+SEND=";
+    char buff2[5];
+    uint16_to_str(buff2, 5, 1);
+    char buff3[] = "\r\n";
+
+    char out[50];
+    char *parts[] = { buff1, buff2, buff3 };
+    join_buffers(parts, 3, out, 50);
+    log_debug(out);
+    
+    
+    
     LED_EN_SetHigh();
     log_debug("Hello from the application!");
     lora_enable();
     
-    double data[] = {123.456};
+    int32_t data[] = {12347};
     lora_send(32, data, 1);
     
     LED_EN_SetLow();
+    set_sleep_period(FIFTEEN_SECONDS);
 }
 
 int main(void) {
